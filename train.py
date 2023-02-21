767f5195b5efd32b55eb564066d89ee4183dce90
mahautm <mateo.mahaut@gmail.com>
2023-02-21 20:11:17 +0100

    initial env refactor and debug

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from llm_com_env import LLMComEnv
 from data import FruitDataset
 
 def prepare_env(config):
-    # Load the model
+    # Load the model TODO: use lamorel
     model = AutoModelForCausalLM.from_pretrained(config.model_name)
     # get the tokenizer, setup padding
     tokenizer = AutoTokenizer.from_pretrained(config.model_name)
@@ -18,9 +18,10 @@ def prepare_env(config):
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
     device = "cpu" if config.force_cpu else "cuda"
     env = LLMComEnv(model, tokenizer, dataloader, device, max_length=20)
-    return env
+    return env, model
 
-def test_play(env):
+def test_play(env, model, device="cuda"):
+    # todo deal with device (through lamorel)
     obs = env.reset()
 
     for i in range(30):
@@ -30,13 +31,17 @@ def test_play(env):
 
     env.render() 
 
+def train(env):
+    # use rl4lm nlpo to train
+    pass
+
 if __name__ == "__main__":
     # TODO: make yaml config
     class Config:
         model_name = "gpt2"
         dataset_path = "./data/mindless_dataset_randomized_train.txt"
-        force_cpu = False
+        force_cpu = True
     config = Config()
-    env = prepare_env(config)
-    test_play(env)
+    env, model = prepare_env(config)
+    test_play(env, model)
 
